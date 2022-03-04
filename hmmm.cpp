a148d915d052cf77a65c5ce120b743027b1d115b
Ly Cong Thanh <themast3t@gmail.com>
2022-03-04 15:28:21 +0700

    Crate a test file

---
+++ hmmm.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include <bits/stdc++.h>
+ using namespace std;
+
+void nhapmang(int a[], int n)
+ {
+     for(int i=0; i<n; i++)
+     {
+         cin >> a[i];
+     }
+ }
+
+ void xuatmang(int a[], int n)
+ {
+     for(int i=0; i<n; i++)
+     {
+         cout << a[i] << ' ';
+     }
+ }
+ int main()
+ {
+
+     return 0;
+ }
